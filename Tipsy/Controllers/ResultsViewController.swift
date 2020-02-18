33ae5ef2bb59c83de94471a5e8f342edd3a57ba4
Matthew Kennedy <burnsidemk@gmail.com>
2020-02-17 19:43:41 -0600

    Step 3

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Matthew Kennedy on 2/17/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
